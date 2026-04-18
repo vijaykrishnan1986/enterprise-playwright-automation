da446070df360b9e31f699c85828627d0b5c2bf1
Vijay Krishnan <vijaykrishnan27@gmail.com>
2026-04-18 11:57:24 +0530

    Updated Test to use credentials from test scripts

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -13,7 +13,7 @@ dotenv.config({ path: envPath });
 // This test will navigate to the login page, fill in the username and password, click the login button and verify that the service title is visible on the home page after login
 // The test will use the LoginPage class to interact with the login page and the HomePage class to interact with the home page after login
 // The test will use the expect function to verify that the service title is visible on the home page after login
-test("Login Sales force application", async ({page}) => {
+test.skip("Login Sales force application", async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLoginPage();
     // await loginPage.fillUsername("vijaykrishnan27.d18c44e8b1ac@agentforce.com");
@@ -47,3 +47,23 @@ test.skip("Encrypt and Decrypt test", async ({page}) => {
     console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
     encryptEnvFile();
 })
+
+
+
+test("Login Sales force application using credentials", async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLoginPage();
+    await loginPage.fillUsername("vijaykrishnan27.d18c44e8b1ac@agentforce.com");
+    await loginPage.fillPassword("password27");
+
+    //! Using environment variables to store sensitive information like username and password is a best practice in test automation. This way, you can keep your credentials secure and avoid hardcoding them in your test scripts. In this example, we are using process.env.userid and process.env.password to access the username and password stored in environment variables. Make sure to set these environment variables before running the test.
+    // ! - The exclamation mark after process.env.userid and process.env.password is a TypeScript non-null assertion operator. It tells the TypeScript compiler that you are sure that these environment variables will be defined at runtime, even though they might be undefined at compile time. This is necessary because TypeScript cannot guarantee that these environment variables will always be set, but you as the developer know that they will be provided when the test runs.
+    // await loginPage.fillUsername(process.env.userid!);
+    // await loginPage.fillPassword(process.env.password!);
+
+    // await loginPage.fillUsername(decrypt(process.env.userid!));
+    // await loginPage.fillPassword(decrypt(process.env.password!));
+    const homepage = await loginPage.clickLoginButton();
+    await homepage.expectServiceTitleToBeVisible();
+    await logger.info("Login test passed successfully");
+});
